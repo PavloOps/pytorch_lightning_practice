7c1f4a1728c87adbdb0dcb9826ceca736ca42ff5
pavloOps <numpyme@gmail.com>
2025-10-30 11:53:24 +0300

    exploration of states in Metric accumulation

---
+++ utils/metric_base_setup.py
@@ -0,0 +1,33 @@
+import torch
+from torchmetrics import Metric
+
+
+class ExampleMetric(Metric):
+    def __init__(self):
+        super().__init__()
+        self.add_state(
+            name="counter",
+            default=torch.tensor(0.0),
+            dist_reduce_fx="sum",
+        )
+
+    def update(self, predictions, targets):
+        self.counter = self.counter + torch.sum(predictions == targets)
+
+    def compute(self):
+        return self.counter
+
+
+m = ExampleMetric()
+
+print(m.counter)
+
+m.update(torch.tensor([1, 2, 3]), torch.tensor([1, 2, 4]))
+
+print(m.counter)
+
+print(m.compute())
+
+m.reset()
+
+print(m.counter)
