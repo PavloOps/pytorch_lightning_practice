dd989e4584227401c83d51b5c672b9319fb3fdf4
pavloops <numpyme@gmail.com>
2026-03-20 17:13:28 +0300

    inits for modules

---
+++ final_project/src/modeling/__init__.py
@@ -0,0 +1,4 @@
+from final_project.src.modeling.two_tower_model import TwoTowerModel
+from final_project.src.modeling.trainer_factory import create_trainer
+
+__all__ = ["TwoTowerModel", "create_trainer"]
