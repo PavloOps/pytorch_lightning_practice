968126df2c1fbf7a214249c184c267a22be7fc5f
Pavlova <data_analitik@s-zdorovie.ru>
2026-07-15 11:13:09 +0300

    removed files from old version of project

--- final_project/debug_samples/__init__.py
+++
@@ -1,9 +0,0 @@
-from final_project.debug_samples.debug_dataset import DebugSetConfig, build_debug_dataset
-from final_project.debug_samples.new_user_slice import NewUserSliceConfig, build_new_user_slice
-
-__all__ = [
-    "DebugSetConfig",
-    "NewUserSliceConfig",
-    "build_debug_dataset",
-    "build_new_user_slice",
-]
--- final_project/src/modeling/__init__.py
+++
@@ -1,4 +0,0 @@
-from final_project.src.modeling.two_tower_model import TwoTowerModel
-from final_project.src.modeling.trainer_factory import create_trainer
-
-__all__ = ["TwoTowerModel", "create_trainer"]
